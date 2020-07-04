684a49bdf98c1c655d19faf453dc7fa481bcf9c1
Ariel Garcia <ga.arielgarcia@gmail.com>
2020-07-04 12:23:01 -0300

    Consultando API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
 import React from 'react';
+import styled from '@emotion/styled';
 
-// API = http://breaking-bad-quotes.herokuapp.com/v1/quotes
+
+const Contenedor = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column;
+`
+
+const Boton = styled.button`
+  background: -webkit-linear-gradient(top left, #007d35 0%, #007d35 40%, #0f574e 100%);
+  background-size: 300px;
+  font-family: Arial, Helvetica, sans-serif;
+  color: #fff;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border: 2px solid black;
+`
 
 function App() {
-  return (<h1>Breaking Bad</h1>);
+
+  const consultarAPI = async () => {
+    const api = await fetch('http://breaking-bad-quotes.herokuapp.com/v1/quotes');
+    const frase =await api.json();
+    console.log(frase[0]);
+  }
+
+  return (
+    <Contenedor>
+      <Boton
+        onClick={consultarAPI}
+      >
+        Obtener frase
+      </Boton>
+    </Contenedor>
+  );
 }
 
 export default App;
